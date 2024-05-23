45ed1142f664aff55aee3851d5ccbbb9cee816cf
ibrawidadi <ibrawidadieuh2003@gmail.com>
2024-05-24 00:28:31 +0100

    ibrapush

--- app/src/main/java/com/example/smarty/SignUpActivity.java
+++ app/src/main/java/com/example/smarty/SignUpActivity.java
@@ -1,4 +1,112 @@
+// SignUpActivity.java
 package com.example.smarty;
 
-public class SignUpActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    private EditText mEmailField, mPasswordField, mNameField, mTelField;
+    private Button mSignUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        mEmailField = findViewById(R.id.emailEditText);
+        mPasswordField = findViewById(R.id.passwordEditText);
+        mNameField = findViewById(R.id.nameEditText);
+        mTelField = findViewById(R.id.phoneEditText);
+        mSignUpButton = findViewById(R.id.signUpButton);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+
+        // Redirection vers LoginActivity
+        TextView loginRedirectText = findViewById(R.id.login_redirect_text);
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void signUp() {
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+        String name = mNameField.getText().toString();
+        String tel = mTelField.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(name) || TextUtils.isEmpty(tel)) {
+            Toast.makeText(SignUpActivity.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String userId = email; // Utiliser l'e-mail comme ID utilisateur
+                            saveUserDataToFirestore(userId, name, tel);
+                            Toast.makeText(SignUpActivity.this, "User created successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "User creation failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void saveUserDataToFirestore(String email, String name, String tel) {
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("name", name);
+        userData.put("tel", tel);
+
+        db.collection("user").document(email) // Utiliser l'e-mail comme ID du document
+                .set(userData)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("SignUpActivity", "User data added to Firestore successfully");
+                        } else {
+                            Log.w("SignUpActivity", "Error adding user data to Firestore", task.getException());
+                        }
+                    }
+                });
+    }
 }
