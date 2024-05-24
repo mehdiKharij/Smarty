418e5ce2f1accaecfaa9ffff05211c49d704a834
FadwaLacham <128438153+FadwaLacham@users.noreply.github.com>
2024-05-24 22:00:15 +0200

    realtime database

--- app/src/main/java/com/example/smarty/KitchenDetailsActivity.java
+++ app/src/main/java/com/example/smarty/KitchenDetailsActivity.java
@@ -1,4 +1,62 @@
 package com.example.smarty;
 
-public class KitchenDetailsActivity {
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class KitchenDetailsActivity extends AppCompatActivity {
+
+    private TextView tvGasDetected;
+    private TextView tvGasValue;
+    private TextView tvFlameDetected;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitchen_details);
+
+        tvGasDetected = findViewById(R.id.tvGasDetected);
+        tvGasValue = findViewById(R.id.tvGasValue);
+        tvFlameDetected = findViewById(R.id.tvFlameDetected);
+
+        // Initialisation des références de la base de données
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("smart-Home");
+
+        databaseReference.child("Gas").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean gasDetected = dataSnapshot.child("gasDetected").getValue(Boolean.class);
+                int gasValue = dataSnapshot.child("value").getValue(Integer.class);
+
+                tvGasDetected.setText("Gas Detected: " + (gasDetected ? "Yes" : "No"));
+                tvGasValue.setText("Gas Value: " + gasValue);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+        databaseReference.child("flame").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean flameDetected = dataSnapshot.child("flameDetected").getValue(Boolean.class);
+                tvFlameDetected.setText("Flame Detected: " + (flameDetected ? "Yes" : "No"));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+    }
 }
